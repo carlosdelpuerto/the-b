29592f551d66b853b84f0384ddeb1fe1e856e972
carlosdelpuerto <carlosegdp@gmail.com>
2022-08-29 12:57:21 -0400

    ig

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,41 @@
 import React from "react";
 import instagram from "../assets/instagram.ico";
 
-
 const Footer = () => {
   return (
-    <div 
-    name='contacto'
-    className="bg-black text-center flex content-around font-bold absolute w-[100%] h-[150px]">
-
-      <div className="flex justify-start"> 
-      <label for="">
-        <a className="h-[50px] w-[50px]"  href="https://www.instagram.com/thebcursos/" target="_blank" rel="noreferrer" >
-          {" "}
-          <img
-            src={instagram}
-            alt="icon"
-            className="h-[50px] w-[50px] mt-6 ml-2"
-          />
-        </a>
-        
-        <div className="text-white flex flexjustify-start mt-3 ml-1">
-        <p className="text-gray-500">EMAIL : <span className="text-white font-light"> thebcursos@gmail.com </span></p>
-        </div>
-
-        <div className="text-gray-700 text-ms flex justify-end font-light ml-1 mt-3">
-        <p>Tembiapo ojejapova mborayhupe ®2022 The-B</p>
-        </div>
-
-      </label>
+    <div
+      name="contacto"
+      className="bg-black text-center flex flex-col content-center font-bold absolute w-[100%] h-[150px]"
+    >
+      {/* El tema del width 100% del instagram solucioné apartandole en un div y centrando en justify, ya que el content tiene el padre */}
+      <div className="flex justify-center">
+        <label for="">
+          <a
+            className=""
+            href="https://www.instagram.com/thebcursos/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <img
+              src={instagram}
+              alt="icon"
+              className="h-[50px] w-[50px] mt-6 ml-2"
+            />
+          </a>
+        </label>
       </div>
 
+      <p className="text-gray-500">
+        EMAIL :{" "}
+        <span className="text-white font-light"> thebcursos@gmail.com </span>
+      </p>
 
+      <div className="text-gray-700 text-ms  font-light mt-5">
+        <p>Tembiapo ojejapova mborayhupe ®2022 The-B</p>
+      </div>
     </div>
   );
 };
-
-
 
 export default Footer;
